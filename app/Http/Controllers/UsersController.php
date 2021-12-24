055523300a9234a4acaef2f86d8ab06c0f01f5a0
Thanh Khan <k.coder.282@gmail.com>
2021-12-25 00:44:17 +0700

    add

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->username;
+        $user->fullname = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->bird = $request->bird;
+        $user->sex = $request->sex;
+        return ["result"=>$user->save()];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->username;
+        $user->fullname = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->bird = $request->bird;
+        $user->sex = $request->sex;
+        return ["result" => $user->save()];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+    }
+
+    public function Login(Request $request)
+    {
+        if(Auth::attempt(["name"=>$request->username, "password" => $request->password]))
+        {
+            return Auth::user();
+        }
+        else return ["result"=>false];
+    }
+    public function Logout($id)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LessonsController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/auth', UsersController::class);
+Route::post('/login', [UsersController::class,'Login']);
 Route::apiResource('/lessons', LessonsController::class);
