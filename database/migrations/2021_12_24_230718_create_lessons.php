73fac27fdd03bb2e39a47405ebd0416d7e9cbda2
Thanh Khan <k.coder.282@gmail.com>
2021-12-24 23:19:11 +0700

    first commit

---
+++ database/migrations/2021_12_24_230718_create_lessons.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("img");
+            $table->string("content");
+            $table->integer("view")->default(0);
+            $table->bigInteger("id_user")->unsigned();
+            $table->foreign("id_user")->references("id")->on("users")->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
